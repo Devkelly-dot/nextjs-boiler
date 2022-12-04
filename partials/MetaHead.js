73fedf221f0895108e407a67b682a747915e146d
2toesxtra <devkelly9622@gmail.com>
2022-12-04 06:53:21 -0500

    home page content fetch

--- partials/MetaHead.js
+++ partials/MetaHead.js
@@ -7,6 +7,12 @@ export default function MetaHead(props)
             {
                 props.title?<title>{props.title}</title>:(<title>Site Name</title>)
             }
+            <meta charSet="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+            <meta name="description" content="Simple landind page" />
+            <meta name="keywords" content="" />
+            <meta name="author" content="" />
         </Head>
     )   
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./partials/**/*.{js,ts,jsx,tsx}",
-    "./layouts/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx,json}",
+    "./partials/**/*.{js,ts,jsx,tsx,json}",
+    "./layouts/**/*.{js,ts,jsx,tsx,json}",
   ],
   theme: {
     extend: {},
