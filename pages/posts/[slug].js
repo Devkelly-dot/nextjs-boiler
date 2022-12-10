b6e1f86e326556f6f70a2daff95e0013fa840f0f
2toesxtra <devkelly9622@gmail.com>
2022-12-10 03:44:04 -0500

    blog page loads

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,5 +1,55 @@
+import authFetch from '../../lib/authFetch'
+import MetaHead from "../../partials/MetaHead"
+import BlogBody from '../../partials/BlogBody'
+
 export default function BlogPost(props)
 {
+    return (
+        <>
+            <MetaHead title={props.post.title}/>
+            <h1>{props.post.title}</h1>
+            <BlogBody blog_body = {props.post.body}/>
+        </>
+    )
+}
+
+export async function getStaticPaths()
+{
+    let url = process.env.NEXT_PUBLIC_REACT_APP_API+`homeContent/?type=blog.BlogPage`
+    const posts = await authFetch(url,{}).then((response)=>response.json())
+    let post_titles = []
+    //build post title array
+    for(let i in posts.items)
+    {
+        post_titles.push({params:{slug:posts.items[i].meta.slug}})
+    }
+
+    return {
+        paths:post_titles,
+        fallback:'blocking'
+    }
 
 }
 
+export async function getStaticProps(context)
+{
+    const slug = context.params.slug;
+    let url = process.env.NEXT_PUBLIC_REACT_APP_API+`homeContent/?slug=`+slug;
+    const post_gen = await authFetch(url,{}).then((response)=>response.json())
+    let id = 0
+    if(post_gen.items.length>0)
+    {
+        id = post_gen.items[0].id
+    }
+    else 
+    {
+        return false 
+    }
+
+    url = process.env.NEXT_PUBLIC_REACT_APP_API+`homeContent/${id}`;
+    const post = await authFetch(url,{}).then((response)=>response.json())
+
+    return {
+        props: {post:post}
+    }
+}
