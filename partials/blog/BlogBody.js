0f069f7193347c99d11763c820d95f34d8ae6a0d
2toesxtra <devkelly9622@gmail.com>
2022-12-12 06:20:56 -0500

    table of contents

--- partials/blog/BlogBody.js
+++ partials/blog/BlogBody.js
@@ -1,5 +1,6 @@
 export default function BlogBody(props)
 {
+    let tableOfContents = []
     function generate_body()
     {
         const class_attributes = {
@@ -27,6 +28,16 @@ export default function BlogBody(props)
                 body+=`<img src="${props.blog_body[i].value}" class="${class_attributes[type].tag}"/>`
                 continue
             }
+            if(type==='heading')
+            {
+                const re = new RegExp('<b>.*</b>')
+                let table_item = re.exec(props.blog_body[i].value)[0]
+                table_item = table_item.replace('<b>','')
+                table_item = table_item.replace('</b>','')
+                tableOfContents.push(table_item)
+                body+=class_attributes[type].tag.replace('>',' class="'+class_attributes[type].className+'"id="'+table_item+'">'+props.blog_body[i].value)
+                continue
+            }
             body+=class_attributes[type].tag.replace('>',' class="'+class_attributes[type].className+'">'+props.blog_body[i].value)
         }
 
@@ -37,6 +48,16 @@ export default function BlogBody(props)
     
     return (
         <>
+            <div className = "border-2 p-4">
+                <h2 className="font-bold text-xl mb-2">Table of Contents</h2>
+                <ol className = 'flex flex-col list-disc gap-y-2 mb-2 pl-3'>
+                    {
+                        tableOfContents.map((content)=>
+                            <li key={"toc:"+content}><a href={"#"+content} className='text-blue-600 underline'>{content}</a></li>
+                        )
+                    }
+                </ol>
+            </div>
             <div dangerouslySetInnerHTML={{__html:body}}></div>
         </>
     )
